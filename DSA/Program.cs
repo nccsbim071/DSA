4f511b8480d3a26c4df2bc97288ef567c5380047
Randhir Kumar Yadav <yrandhir@outlook.com>
2019-05-17 18:46:14 +0545

    Added source code for Data Structure and Algorithm project.

---
+++ DSA/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DSA
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            //Enqueue
+            queue.Enqueue(6);
+            queue.Enqueue(2);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Enqueue(12);
+            queue.DisplayQueue();
+
+            //Dequeue
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.DisplayQueue();
+
+            //Enqueue
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+            queue.DisplayQueue();
+
+            int? temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+
+            //Enqueue
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.DisplayQueue();
+
+
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");
+            temp = queue.Dequeue();
+            Console.WriteLine($"Dequeue: {(temp.HasValue ? temp.Value.ToString() : string.Empty)}");            
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ DSA/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA
+{
+    public class Queue
+    {
+        int size = 5;
+        int?[] myArray = new int?[5];
+        int front = 0;
+        int rear = -1;
+        int itemCount = 0;
+        public int? Peek()
+        {
+            return myArray[front];
+        }
+        public bool IsFull()
+        {
+            return itemCount == size;
+        }
+        public bool IsEmpty()
+        {
+            return itemCount == 0;
+        }
+        public void Enqueue(int data)
+        {
+            if (!IsFull())
+            {
+                if (rear == size - 1)
+                {
+                    rear = -1;
+                }
+
+                rear += 1;
+                myArray[rear] = data;
+                itemCount += 1;
+            }
+            else
+            {
+                Console.WriteLine("Cannot enqueue. Queue is full.");
+            }
+        }
+        public int? Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                int? temp = myArray[front];
+                myArray[front] = null;
+                front += 1;
+                if (front == size)
+                {
+                    front = 0;
+                }
+
+                itemCount -= 1;
+
+                return temp;
+            }
+            else
+            {
+                Console.WriteLine($"Cannot dequeue. Queue is empty.");
+                return null;
+            }
+        }
+
+        public void DisplayQueue()
+        {
+            Console.WriteLine($"Queue: {string.Join(',', myArray)}");
+        }
+    }
+}
