a80de47f757470aafa0675eebb096105c3ecd7d8
Randhir Kumar Yadav <yrandhir@outlook.com>
2019-05-18 16:51:18 +0545

    Added code or selection sort.

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -22,10 +22,22 @@ namespace DSA
             //a = InsertionSort.SortAscending(a);
             //Console.WriteLine($"Output: Array: {String.Join(",", a)}");
 
-            int[] a1 = new int[] { 15, 1, 18, 3, 45 };
-            Console.WriteLine($"Input Array: {String.Join(",", a1)}");
-            a1 =  InsertionSort.SortDescending(a1);
-            Console.WriteLine($"Output Array: {String.Join(",", a1)}");
+            //int[] a1 = new int[] { 15, 1, 18, 3, 45 };
+            //Console.WriteLine($"Input Array: {String.Join(",", a1)}");
+            //a1 =  InsertionSort.SortDescending(a1);
+            //Console.WriteLine($"Output Array: {String.Join(",", a1)}");
+
+            //int[] a = new int[] { 15, 9, 23, 10, 6 };
+            //Console.WriteLine($"Sort Ascending");
+            //Console.WriteLine($"Input Array: {String.Join(",", a)}");
+            //a = SelectionSort.SortAscending(a);
+            //Console.WriteLine($"Output Array: {String.Join(",", a)}");
+
+            //a = new int[] { 15, 9, 23, 10, 6 };
+            //Console.WriteLine("\n\nSort Descending");
+            //Console.WriteLine($"Input Array: {String.Join(",", a)}");
+            //a = SelectionSort.SortDescending(a);
+            //Console.WriteLine($"Output Array: {String.Join(",", a)}");
 
             Console.ReadKey();
         }
---
+++ DSA/Sorting_Techniques/SelectionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Sorting_Techniques
+{
+    public class SelectionSort
+    {
+        public static int[] SortAscending(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int smallestElementPosition = i;
+
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] < a[smallestElementPosition])
+                    {
+                        smallestElementPosition = j;
+                    }
+                }
+
+                if (smallestElementPosition != i)
+                {
+                    int temp = a[i];
+                    a[i] = a[smallestElementPosition];
+                    a[smallestElementPosition] = temp;
+                }
+            }
+
+            return a;
+        }
+
+        public static int[] SortDescending(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                int smallestElementPosition = i;
+
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[j] > a[smallestElementPosition])
+                    {
+                        smallestElementPosition = j;
+                    }
+                }
+
+                if (smallestElementPosition != i)
+                {
+                    int temp = a[i];
+                    a[i] = a[smallestElementPosition];
+                    a[smallestElementPosition] = temp;
+                }
+            }
+
+            return a;
+        }
+    }
+}
