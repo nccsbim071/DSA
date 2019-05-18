4d6ada7b3ce1e6a3f093d120aed8d0886e5fbe7d
Randhir Kumar Yadav <yrandhir@outlook.com>
2019-05-18 16:15:06 +0545

    Added code for InsertionSort method.

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -10,12 +10,22 @@ namespace DSA
             //int[] a = new int[] { 9, 3, 2, 1, 0 };
             //Console.WriteLine($"Input Array: {String.Join(",", a)}");
             //a = BubbleSort.SortAscending(a);
-            //Console.WriteLine($"Input Array: {String.Join(",", a)}");
+            //Console.WriteLine($"Output Array: {String.Join(",", a)}");
 
             //int[] a = new int[] { 15, 1, 18, 3, 45 };
             //Console.WriteLine($"Input Array: {String.Join(",", a)}");
             //a = BubbleSort.SortDescending(a);
-            //Console.WriteLine($"Input Array: {String.Join(",", a)}");
+            //Console.WriteLine($"Output Array: {String.Join(",", a)}");
+
+            //int[] a = new int[] { 15, 1, 18, 3, 45 };
+            //Console.WriteLine($"Input: Array: {String.Join(",", a)}");
+            //a = InsertionSort.SortAscending(a);
+            //Console.WriteLine($"Output: Array: {String.Join(",", a)}");
+
+            int[] a1 = new int[] { 15, 1, 18, 3, 45 };
+            Console.WriteLine($"Input Array: {String.Join(",", a1)}");
+            a1 =  InsertionSort.SortDescending(a1);
+            Console.WriteLine($"Output Array: {String.Join(",", a1)}");
 
             Console.ReadKey();
         }
---
+++ DSA/Sorting_Techniques/InsertionSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Sorting_Techniques
+{
+    public class InsertionSort
+    {
+        public static int[] SortAscending(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int valueToInsert = a[i];
+                int holePosition = i;
+
+                while (holePosition > 0 && a[i - 1] > valueToInsert)
+                {
+                    a[holePosition] = a[holePosition - 1];
+                    holePosition--;
+                }
+
+                if (holePosition != i)
+                {
+                    a[holePosition] = valueToInsert;
+                }
+            }
+
+            return a;
+        }
+
+        public static int[] SortDescending(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                int valueToInsert = a[i];
+                int holePosition = i;
+
+                while (holePosition > 0 && a[holePosition - 1] < valueToInsert)
+                {
+                    a[holePosition] = a[holePosition - 1];
+                    holePosition--;
+                }
+
+                if (holePosition != i)
+                {
+                    a[holePosition] = valueToInsert;
+                }
+            }
+
+            return a;
+        }
+    }
+}
